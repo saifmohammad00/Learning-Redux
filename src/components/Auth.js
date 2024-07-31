02a477b728778448feeaaf0752aef1613afbe8a4
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-31 14:43:31 +0530

    splitting redux file

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,5 +1,5 @@
 import classes from './Auth.module.css';
-import { authActions } from '../store/index';
+import { authActions } from '../store/auth';
 import { useDispatch } from 'react-redux';
 
 const Auth = () => {
